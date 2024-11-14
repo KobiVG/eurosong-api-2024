9ab69ee28a24d5adcec43e98484b20343f954490
KobiVG <kobevangestel@outlook.com>
2024-11-14 16:53:58 +0100

    Made counter component in app.vue

--- eurosong-frontend/src/App.vue
+++ eurosong-frontend/src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <CounterComponent></CounterComponent>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CounterComponent from './components/CounterComponent.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    CounterComponent
   }
 }
 </script>
---
+++ eurosong-frontend/src/components/CounterComponent.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h2>
+            8
+        </h2>
+        <button>
+            +
+        </button>
+        <button>
+            -
+        </button>
+    </div>
+</template>
+
+
+<script>
+    export default {
+        name: 'CounterComponent'
+    }
+</script>
